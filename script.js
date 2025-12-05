3617144d7da0c0db8d60f937f7aba464453677e5
NoemiMassucco <noemimassucco@gmail.com>
2025-12-05 20:44:36 +0100

    Dichiarazione e inizializzazione const

---
+++ script.js
@@ -0,0 +1,23 @@
+//Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+//il prezzo del biglietto è definito in base ai km (0.21 € al km),
+const priceTiketForKm = 0.21;
+//va applicato uno sconto del 20% per i minorenni,
+const discountYoung = 0.2;
+//va applicato uno sconto del 40% per gli over 65.,
+const discountOlder = 0.4;
+//L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+// Questo richiederà un minimo di ricerca.,
+
+const 
+
+
+
+
+TIP:
+//per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
+
+//100km, 10 anni => prezzo corretto:  €16.80,
+//100km, 70 anni => prezzo corretto: €12.60,
+
